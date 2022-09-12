4187f5acf2e443c714b177c204854566f91db781
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-09-12 13:29:17 +0400

    Auto stash before merge of "master" and "origin/master"

---
+++ cv/assets/scripts/main.js
@@ -0,0 +1,5 @@
+// This script should be optional, and critical parts of the page should work without it
+
+window.addEventListener('load', (event) => {
+  document.body.classList.remove('loading');
+});
