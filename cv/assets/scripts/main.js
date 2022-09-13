70845ffc6dd35f78aea196d9e9631e108d8be0bb
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-09-13 18:23:06 +0400

    wip: temporary commit

--- cv/assets/scripts/main.js
+++ cv/assets/scripts/main.js
@@ -1,8 +1,26 @@
 // This script should be optional, and critical parts of the page should work without it
 
-const onLoad = () => {
-  document.body.classList.remove('loading');
+class OnLoadHandler {
+  constructor() {
+    let handled = false
+    let timer = undefined
+
+    const handleOnce = () => {
+      if (!handled) {
+        handled = true
+
+        window.clearTimeout(timer)
+        this.handle()
+      }
+    }
+
+    window.addEventListener('load', handleOnce)
+    timer = window.setTimeout(handleOnce, 3500) // used as a fallback
+  }
+
+  handle() {
+    document.body.classList.remove('loading')
+  }
 }
 
-window.addEventListener('load', onLoad);
-window.setTimeout(onLoad, 3500); // fallback
+new OnLoadHandler()
