2a70660e5f44d73eff5e0846e7b8b3cfd44ccc64
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-09-14 14:27:29 +0400

    Auto stash before merge of "master" and "origin/master"

--- cv/assets/scripts/main.js
+++ cv/assets/scripts/main.js
@@ -1,26 +1,2 @@
 // This script should be optional, and critical parts of the page should work without it
 
-class OnLoadHandler {
-  constructor() {
-    let handled = false
-    let timer = undefined
-
-    const handleOnce = () => {
-      if (!handled) {
-        handled = true
-
-        window.clearTimeout(timer)
-        this.handle()
-      }
-    }
-
-    window.addEventListener('load', handleOnce)
-    timer = window.setTimeout(handleOnce, 3500) // used as a fallback
-  }
-
-  handle() {
-    document.body.classList.remove('loading')
-  }
-}
-
-new OnLoadHandler()
