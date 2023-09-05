351b872ca614c1ef85e974bdae1421a7099934ba
Pаramtamtām <7326800+tarampampam@users.noreply.github.com>
2023-09-05 08:37:14 -0700

    Feat: Add codespace page&worker (#10)

---
+++ code/README.md
@@ -0,0 +1,8 @@
+## Custom CF worker for `code.tarampamp.am`
+
+Hosted on [CloudFlare Pages](https://pages.cloudflare.com/).
+
+Useful links:
+
+- [Routing](https://developers.cloudflare.com/pages/platform/functions/routing/)
+- [API reference](https://developers.cloudflare.com/pages/platform/functions/api-reference/)
---
+++ code/functions/vm/off.js
@@ -0,0 +1,27 @@
+export async function onRequestPost(context) {
+  /** @type {{HCLOUD_API_KEY: string, HCLOUD_CODESPACE_SERVER_ID: string}} */
+  const env = context.env
+
+  try {
+    await fetch( // https://docs.hetzner.cloud/#server-actions-power-off-a-server
+      new Request(`https://api.hetzner.cloud/v1/servers/${env.HCLOUD_CODESPACE_SERVER_ID}/actions/poweroff`),
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${env.HCLOUD_API_KEY}`,
+        },
+      },
+    )
+
+    return new Response(
+      new Blob([JSON.stringify({ success: true }, null, 2)], { type: 'application/json' }),
+      { status: 200 },
+    )
+  } catch (err) {
+    return new Response(
+      new Blob([JSON.stringify({ error: err }, null, 2)], { type: 'application/json' }),
+      { status: 500 },
+    )
+  }
+}
