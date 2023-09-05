436c167dff0f43baaaca80030d974d763cd44819
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-09-06 00:39:55 +0400

    wip: 🔕 temporary commit

--- code/functions/_middleware.js
+++ code/functions/_middleware.js
@@ -1,11 +1,15 @@
 const verifyTurnstileToken = async ({ request, next, env }) => {
-  const tt = request.headers.get('X-Tt')
+  const url = new URL(request.url)
 
-  if (!tt) {
-    return new Response(
-      new Blob([JSON.stringify({ error: 'No token header was passed' }, null, 2)], { type: 'application/json' }),
-      { status: 403 },
-    )
+  if (url.pathname.startsWith('/vm')) {
+    const tt = request.headers.get('X-Tt')
+
+    if (!tt) {
+      return new Response(
+        new Blob([JSON.stringify({ error: 'No token header was passed' }, null, 2)], { type: 'application/json' }),
+        { status: 403 },
+      )
+    }
   }
 
   return next()
