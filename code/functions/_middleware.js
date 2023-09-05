726e92377c1f5f8ffef1278d62cd40897c6c97d0
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-09-06 00:49:42 +0400

    wip: 🔕 temporary commit

--- code/functions/_middleware.js
+++ code/functions/_middleware.js
@@ -6,8 +6,35 @@ const verifyTurnstileToken = async ({ request, next, env }) => {
 
     if (!tt) {
       return new Response(
-        new Blob([JSON.stringify({ error: 'No token header was passed' }, null, 2)], { type: 'application/json' }),
+        new Blob([JSON.stringify({ error: 'No token header' }, null, 2)], { type: 'application/json' }),
         { status: 403 },
+      )
+    }
+
+    let formData = new FormData()
+
+    formData.append('secret', env.TURNSTILE_SECRET_KEY)
+    formData.append('response', tt)
+    formData.append('remoteip', request.headers.get('CF-Connecting-IP'))
+
+    try { // https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
+      const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+        body: formData,
+        method: 'POST',
+      })
+
+      const data = await result.json()
+
+      if (!data.success) {
+        return new Response(
+          new Blob([JSON.stringify({ error: 'Wrong token' }, null, 2)], { type: 'application/json' }),
+          { status: 403 },
+        )
+      }
+    } catch (err) {
+      return new Response(
+        new Blob([JSON.stringify({ error: err.toString() }, null, 2)], { type: 'application/json' }),
+        { status: 500 },
       )
     }
   }
