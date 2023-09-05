28615b13bf61ba370ec3a3259180f68f3586199a
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-09-05 19:06:30 +0400

    wip: 🔕 temporary commit

--- code/functions/vm/index.js
+++ code/functions/vm/index.js
@@ -2,14 +2,28 @@ export async function onRequestGet(context) {
   /** @type {{HCLOUD_API_KEY: string, HCLOUD_CODESPACE_SERVER_ID: string}} */
   const env = context.env
 
-  // https://docs.hetzner.cloud/#servers-get-a-server
+  try {
+    const resp = await fetch( // https://docs.hetzner.cloud/#servers-get-a-server
+      new Request(`https://api.hetzner.cloud/v1/servers/${env.HCLOUD_CODESPACE_SERVER_ID}`),
+      {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: `Bearer ${env.HCLOUD_API_KEY}`,
+        },
+      },
+    )
 
-  return new Response(
-    new Blob([JSON.stringify({
-      server_id: env.HCLOUD_CODESPACE_SERVER_ID,
-    }, null, 2)], { type: 'application/json' }),
-    {
-      status: 200,
-    },
-  )
+    const data = await resp.json()
+
+    return new Response(
+      new Blob([JSON.stringify({ status: data.server.status }, null, 2)], { type: 'application/json' }),
+      { status: 200 },
+    )
+  } catch (err) {
+    return new Response(
+      new Blob([JSON.stringify({ error: err }, null, 2)], { type: 'application/json' }),
+      { status: 500 },
+    )
+  }
 }
