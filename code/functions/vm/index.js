9f851a9356cd1086165f5be424148077ea2ab76c
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-09-05 18:39:15 +0400

    wip: 🔕 temporary commit

--- code/functions/vm/index.js
+++ code/functions/vm/index.js
@@ -1,3 +1,10 @@
 export async function onRequestGet(context) {
-  return new Response('Action: STATUS')
+  return new Response(
+    new Blob([JSON.stringify({
+      foo: 'bar',
+    }, null, 2)], { type: 'application/json' }),
+    {
+      status: 200,
+    },
+  )
 }
