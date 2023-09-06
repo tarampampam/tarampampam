b57558503ce3e848fe4f1fac8fdb4d16d25f1ec3
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-09-06 11:36:30 +0400

    wip: 🔕 temporary commit

--- code/scripts/index.js
+++ code/scripts/index.js
@@ -1,54 +1,38 @@
-/** @type {String|null} */
-let turnstileToken = null
+/** @type {HTMLPictureElement} */
+const logo = document.getElementById('logo')
+/** @type {HTMLDivElement} */
+const state = document.getElementById('state')
+/** @type {HTMLInputElement} */
+const sw = document.getElementById('state-switch')
+
 const hiddenClass = 'hidden'
 
-window.turnstileLoaded = () => {
+logo.addEventListener('dblclick', async (e) => {
   try {
-    turnstile.render('#tt', {
-      sitekey: '0x4AAAAAAAJwE0fGOLs_7cZ0',
-      appearance: 'interaction-only',
-      callback: (token) => { turnstileToken = token },
-    })
+    if (state.classList.contains(hiddenClass)) {
+      const status = (await (await fetch('vm')).json()).status
+
+      console.log(status)
+
+      sw.checked = status === 'running'
+    }
+
+    state.classList.toggle(hiddenClass)
   } catch (err) {
-    iziToast.error({ title: 'Turnstile error', message: err.toString() })
+    iziToast.error({ title: 'Error', message: err.toString() })
   }
+}, false)
 
-  /** @type {HTMLPictureElement} */
-  const logo = document.getElementById('logo')
-  /** @type {HTMLDivElement} */
-  const state = document.getElementById('state')
-  /** @type {HTMLInputElement} */
-  const sw = document.getElementById('state-switch')
-
-  logo.addEventListener('dblclick', async (e) => {
-    try {
-      if (state.classList.contains(hiddenClass)) {
-        const status = (await (await fetch('vm', {
-          headers: { 'X-Tt': turnstileToken },
-        })).json()).status
-
-        console.log(status)
-
-        sw.checked = status === 'running'
-      }
-
-      state.classList.toggle(hiddenClass)
-    } catch (err) {
-      iziToast.error({ title: 'Error', message: err.toString() })
+sw.addEventListener('change', async (e) => {
+  try {
+    if (sw.checked) {
+      await fetch('vm/on', { method: 'POST' })
+    } else {
+      await fetch('vm/off', { method: 'POST' })
     }
-  }, false)
 
-  sw.addEventListener('change', async (e) => {
-    try {
-      if (sw.checked) {
-        await fetch('vm/on', { method: 'POST', headers: { 'X-Tt': turnstileToken } })
-      } else {
-        await fetch('vm/off', { method: 'POST', headers: { 'X-Tt': turnstileToken } })
-      }
-
-      window.setTimeout(state.classList.toggle(hiddenClass), 1500)
-    } catch (err) {
-      iziToast.error({ title: 'Error', message: err.toString() })
-    }
-  }, false)
-}
+    window.setTimeout(state.classList.toggle(hiddenClass), 1500)
+  } catch (err) {
+    iziToast.error({ title: 'Error', message: err.toString() })
+  }
+}, false)
